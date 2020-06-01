b281c151f3ff69ff9c311b027d9f5d4f276a7f34
Yinlung Kwok <yinlung.kwok@cognizant.com>
2020-06-01 13:52:11 -0400

    initial

---
+++ src/main/java/com/example/gcloudiapdemo/GcloudIapDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gcloudiapdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcloudIapDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcloudIapDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gcloudiapdemo/GcloudIapDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gcloudiapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcloudIapDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
